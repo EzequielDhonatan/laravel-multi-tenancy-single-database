d30cea9b1ecc2315f71deb3c330fd0b9ae9d87c0
Ezequiel Dhonatan <Contato@ezequieldhonatan.com.br>
2021-09-08 18:23:48 -0300

    Tenant - Trait

--- app/Models/Panel/Blog/Post/Post.php
+++ app/Models/Panel/Blog/Post/Post.php
@@ -5,24 +5,14 @@ namespace App\Models\Panel\Blog\Post;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Tenant\Traits\TraitTenant;
 use App\Models\User;
-use App\Scopes\Tenant\TenantScope;
-use App\Observers\Tenant\TenantObserver;
 
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory, TraitTenant;
 
     protected $fillable = [ 'user_id', 'title', 'url', 'body', 'situation' ];
-
-    public static function boot()
-    {
-        parent::boot();
-
-        static::addGlobalScope( new TenantScope );
-
-        static::observe( new TenantObserver );
-    }
 
     public function user()
     {
