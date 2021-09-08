3f79a6752a7a0b1675f9ef6f5b12c4bee02d0966
Ezequiel Dhonatan <Contato@ezequieldhonatan.com.br>
2021-09-08 13:45:11 -0300

    Migrations - User (Updated)

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+
+            /* DADOS DO USUÁRIO
+            ================================================== */
+            $table->string( 'name' ); ## NOME
+            $table->string( 'email' )->unique( ); ## E-MAIL
+            $table->timestamp( 'email_verified_at' )->nullable(); ## E-MAIL VERIFICADO EM
+            $table->string( 'password' ); ## SENHA
             $table->rememberToken();
+
             $table->timestamps();
         });
     }
