b0d29a1919ea539ce514493b70f99dff18138982
Ezequiel Dhonatan <Contato@ezequieldhonatan.com.br>
2021-09-08 13:29:09 -0300

    Installation - Laravel 8.6.2

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,17 @@
 <?php
 
-Route::resource('/posts', 'PostController');
+use Illuminate\Support\Facades\Route;
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
