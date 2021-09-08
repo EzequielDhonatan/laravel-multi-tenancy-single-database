0379b5dd8c6d5b629e2b5fdaeb7fc1667247c7ba
Ezequiel Dhonatan <Contato@ezequieldhonatan.com.br>
2021-09-08 17:42:29 -0300

    Tenant - Global Scope

--- app/Models/Panel/Blog/Post/Post.php
+++ app/Models/Panel/Blog/Post/Post.php
@@ -6,12 +6,20 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 use App\Models\User;
+use App\Scopes\Tenant\TenantScope;
 
 class Post extends Model
 {
     use HasFactory;
 
     protected $fillable = [ 'title', 'body', 'user_id', 'situation' ];
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope( new TenantScope );
+    }
 
     public function user()
     {
---
+++ app/Scopes/Tenant/TenantScope.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Scopes\Tenant;
+
+use Illuminate\Database\Eloquent\Scope;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+
+use App\Tenant\Manager\ManagerTenant;
+
+class TenantScope implements Scope
+{
+    public function apply( Builder $builder, Model $model )
+    {
+        $tenant = app( ManagerTenant::class)->getTenantIdentify();
+
+        $builder->where( 'tenant_id', $tenant );
+    }
+
+} // TenantScope
